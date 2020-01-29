b95df9b7c6e11c70407734d13e86611164860134
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-01-28 21:28:00 -0800

    added database configuration file to init connection and load models

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import routes from './routes';
 
+import './database';
+
 require('dotenv').config();
 
 class App {
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,17 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hi Baum show' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Juliani',
+    email: 'juliani@gmail.com',
+    password_hash: '123123',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
