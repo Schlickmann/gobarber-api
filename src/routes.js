ea73c59f89706f4a866ff5a9b29d91c718244d25
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-01-28 21:35:14 -0800

    added UserController and method store

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,9 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Juliani',
-    email: 'juliani@gmail.com',
-    password_hash: '123123',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
