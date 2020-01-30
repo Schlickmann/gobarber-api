c6b03cc47d7effd4c10f33c37b5878b3b2e27795
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-01-29 17:16:25 -0800

    added functionality to authenticate user

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,34 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/User';
+import configAuth from '../../config/auth';
+
+class SessionController {
+  async store(req, res) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(401).json({ error: 'User nor found.' });
+    }
+
+    if (!(await user.checkPassword(password))) {
+      return res.status(401).json({ error: 'Password does not match.' });
+    }
+
+    const { id, name } = user;
+
+    return res.json({
+      user: {
+        id,
+        name,
+        email,
+      },
+      token: jwt.sign({ id }, configAuth.secret, {
+        expiresIn: configAuth.ttl,
+      }),
+    });
+  }
+}
+
+export default new SessionController();
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: process.env.JWT_SECRET,
+  ttl: process.env.JWT_EXPIRES,
+};
