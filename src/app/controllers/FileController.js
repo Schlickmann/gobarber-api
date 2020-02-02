f82d2a868772d93793762005584b12a053e551b5
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-02-02 14:11:30 -0800

    created new migrations to add files table and new column in users table

--- src/app/controllers/FileController.js
+++ src/app/controllers/FileController.js
@@ -1,6 +1,12 @@
+import File from '../models/File';
+
 class FileController {
   async store(req, res) {
-    return res.json({ ok: true });
+    const { originalname: name, filename: path } = req.file;
+
+    const file = await File.create({ name, path });
+
+    return res.json(file);
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import Auth from './app/middlewares/auth';
 
@@ -17,8 +18,6 @@ routes.post('/users', UserController.store);
 routes.use(Auth.isUserAuthenticated);
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
