7c8313e64337199084a68161c1cf58b1fb46238c
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-02-02 15:52:07 -0800

    added functionality to list provider agenda

---
+++ src/app/controllers/ScheduleController.js
@@ -0,0 +1,48 @@
+import * as Yup from 'yup';
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import { Op } from 'sequelize';
+import User from '../models/User';
+import File from '../models/File';
+import Appointment from '../models/Appointment';
+
+class ScheduleController {
+  async index(req, res) {
+    const { page = 1, date } = req.query;
+    const parsedDate = parseISO(date);
+
+    const isProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!isProvider) {
+      return res.status(401).json({ error: 'User is not a provider.' });
+    }
+
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)],
+        },
+      },
+      order: ['date'],
+      limit: page,
+      offset: (page - 1) * 20,
+      attributes: ['id', 'date'],
+      include: [
+        {
+          model: User,
+          as: 'user',
+          attributes: ['id', 'name'],
+          include: [
+            { model: File, as: 'avatar', attributes: ['id', 'path', 'url'] },
+          ],
+        },
+      ],
+    });
+    return res.json(appointments);
+  }
+}
+
+export default new ScheduleController();
--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import Auth from './app/middlewares/auth';
 
@@ -24,7 +25,11 @@ routes.get('/providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
+// User appointments
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+
+// Provider schedule
+routes.get('/schedule', ScheduleController.index);
 
 export default routes;
