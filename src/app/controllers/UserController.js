47db4fa0ff112780abe781dfae6a327a06f7249d
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-02-02 12:21:34 -0800

    changed error message

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -15,7 +15,7 @@ class UserController {
     });
 
     if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({ error: 'Validation fails.' });
+      return res.status(400).json({ error: 'Validation failed.' });
     }
 
     const userExists = await User.findOne({ where: { email: req.body.email } });
@@ -45,7 +45,7 @@ class UserController {
     });
 
     if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({ error: 'Validation fails.' });
+      return res.status(400).json({ error: 'Validation failed.' });
     }
 
     const { email, oldPassword } = req.body;
