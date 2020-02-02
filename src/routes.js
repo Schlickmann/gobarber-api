04f8179473a68666a026f7336aed825577d22804
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-02-02 15:04:47 -0800

    added appointment controller to store new appointments

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import Auth from './app/middlewares/auth';
 
@@ -22,5 +23,7 @@ routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
+
+routes.post('/appointments', AppointmentController.store);
 
 export default routes;
