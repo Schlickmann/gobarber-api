abc81600b6961df61e9f14b3b2d0d2b5e1b14679
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-03-13 20:52:43 -0700

    changed timesheet controller

--- src/app/controllers/TimesheetController.js
+++ src/app/controllers/TimesheetController.js
@@ -1,9 +1,14 @@
 import ProviderSchedule from '../models/ProviderSchedule';
+import Schedule from '../models/Schedule';
 
 class TimesheetController {
   async index(req, res) {
     const timesheet = await ProviderSchedule.findAll({
+      attributes: [],
       where: { provider_id: req.userId },
+      include: [
+        { model: Schedule, as: 'schedule', attributes: ['id', 'time'] },
+      ],
     });
 
     return res.json(timesheet);
